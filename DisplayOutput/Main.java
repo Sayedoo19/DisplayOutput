124c401dbfc3406607d2e9536fdb86d4fec5d416
! Sayed <alawisayed792@gmail.com>
2022-11-09 22:07:58 +0300

    Input and Variables

--- DisplayOutput/Main.java
+++ DisplayOutput/Main.java
@@ -1,14 +1,22 @@
-
+import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //this program to calculate rectangle area
 
-        System.out.println("He said that \"Iam from Egypt\" But lives is \"KSA\" ");
-        System.out.println("He said that \n Iam from Egypt \n But lives is KSA ");
-        System.out.println("The file located in d:\\javaprojects\\tryoutput");
-        System.out.println("Work days are \n Sunday \t Monday \t Tuesday \t Wednesday \t");
+        int length, width, area;
+
+        System.out.println("Enter rectangle length");
+        length = input.nextInt();
+        System.out.println("Enter rectangle width");
+        width = input.nextInt();
+        area = length * width ;
+        System.out.println(area);
+        System.out.printf("The Rectangle area is %d",area);
+
     }
 
 }
