94c6d65a41dc1ffb8602bd9672951aa35a045603
! Sayed <alawisayed792@gmail.com>
2022-11-09 22:27:41 +0300

    IF statement 2

--- DisplayOutput/Main.java
+++ DisplayOutput/Main.java
@@ -6,20 +6,17 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int number;
-        System.out.println("Enter number to test");
-        number = input.nextInt();
-        if (number >=0)
-        {
+        int number1, number2;
+        System.out.println("Enter 2 numbers to see Which number is Greater");
+        number1 = input.nextInt();
+        number2 = input.nextInt();
 
-                    if (number%2 == 0) {
-                        System.out.println("Positive Number");
-                        System.out.println("Even another Number");
-                    }
-                    else System.out.println("Odd Number");
-        }
+        if (number1==number2)
+            System.out.println("Equal");
+        else if (number1>number2)
+            System.out.println("Number1 is greater");
         else
-            System.out.println("Negative Number");
+            System.out.println("Number2 is greater");
 
     }
 }
