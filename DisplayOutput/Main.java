c229560002e2920912380f0397708a0ea71ea2ef
! Sayed <alawisayed792@gmail.com>
2022-11-09 22:10:02 +0300

    Input and Variables 2

--- DisplayOutput/Main.java
+++ DisplayOutput/Main.java
@@ -6,8 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         //this program to calculate rectangle area
+        String fname="";
+        float salary;
+        float net_salary;
+        System.out.println("Enter your first name");
+        fname = input.next();
 
         int length, width, area;
+        System.out.println("Enter your salary");
+        salary = input.nextFloat();
+        net_salary = salary - (salary * 0.10f);
 
         System.out.println("Enter rectangle length");
         length = input.nextInt();
@@ -17,6 +25,7 @@ public class Main {
         System.out.println(area);
         System.out.printf("The Rectangle area is %d",area);
 
+        System.out.printf("your Net salary is %f",net_salary);
     }
 
 }
