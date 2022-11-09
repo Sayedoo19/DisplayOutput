3f2be2c9fe08c5828fd00b6d180946eb12167add
! Sayed <alawisayed792@gmail.com>
2022-11-09 22:22:17 +0300

    IF statement

--- DisplayOutput/Main.java
+++ DisplayOutput/Main.java
@@ -4,38 +4,22 @@ public class Main {
 
 
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-        //this program to calculate rectangle area
-        String fname="";
-        float salary;
-        float net_salary;
-        System.out.println("Enter your first name");
-        fname = input.next();
+        int number;
+        System.out.println("Enter number to test");
+        number = input.nextInt();
+        if (number >=0)
+        {
 
-        System.out.println(2+3 *5);
-        System.out.println(  (2+3) *5);
-
-
-        double x = 5.0, y = 2;
-
-        System.out.println(x/y);
-
-        System.out.println("Enter your salary");
-        salary = input.nextFloat();
-        net_salary = salary - (salary * 0.10f);
-        int i = 9, j = 2;
-        // System.out.println( (float) i/j);
-        //i++;
-        //j--;
-
-        System.out.printf("your Net salary is %f",net_salary);
-        //i+=5;
-        //j*=5;
-        System.out.println(i);
-        System.out.println(j);
-
-        System.out.println(++i * --j * 2);
+                    if (number%2 == 0) {
+                        System.out.println("Positive Number");
+                        System.out.println("Even another Number");
+                    }
+                    else System.out.println("Odd Number");
+        }
+        else
+            System.out.println("Negative Number");
 
     }
-
 }
