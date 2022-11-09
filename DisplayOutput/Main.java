49b3d425eece90d5fe6547c6dae0ce43d9263f9d
! Sayed <alawisayed792@gmail.com>
2022-11-09 22:06:37 +0300

    DisplayOutput

---
+++ DisplayOutput/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("He said that \"Iam from Egypt\" But lives is \"KSA\" ");
+        System.out.println("He said that \n Iam from Egypt \n But lives is KSA ");
+        System.out.println("The file located in d:\\javaprojects\\tryoutput");
+        System.out.println("Work days are \n Sunday \t Monday \t Tuesday \t Wednesday \t");
+    }
+
+}
