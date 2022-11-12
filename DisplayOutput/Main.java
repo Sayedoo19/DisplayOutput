8e14c486c0c4d3008342fc2134dc17f4b1196939
! Sayed <alawisayed792@gmail.com>
2022-11-12 16:57:22 +0300

    Multiway if else statement

--- DisplayOutput/Main.java
+++ DisplayOutput/Main.java
@@ -6,17 +6,21 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int number1, number2;
-        System.out.println("Enter 2 numbers to see Which number is Greater");
-        number1 = input.nextInt();
-        number2 = input.nextInt();
-
-        if (number1==number2)
-            System.out.println("Equal");
-        else if (number1>number2)
-            System.out.println("Number1 is greater");
+        int marks;
+        System.out.println("Enter You Mark");
+        marks = input.nextInt();
+        if (marks >=90)
+            System.out.println("You Got A");
+        else if (marks >=80)
+            System.out.println("You Got B");
+        else if (marks >=70)
+            System.out.println("You Got C");
+        else if (marks >=60)
+            System.out.println("You Got D");
         else
-            System.out.println("Number2 is greater");
+            System.out.println("You Got F");
+
+
 
     }
 }
